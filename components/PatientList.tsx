f61bb16de7774913f464c7feebb839e1d9907c34
Divine Orji <dpkreativ@gmail.com>
2025-02-24 05:14:16 +0100

    removing mock data

--- components/PatientList.tsx
+++ components/PatientList.tsx
@@ -1,38 +1,50 @@
-import SearchIcon from "./icons/SearchIcon"
-import MenuIcon from './icons/MenuIcon';
+import SearchIcon from "./icons/SearchIcon";
+import MenuIcon from "./icons/MenuIcon";
 
-import { patients } from "./utils/patientsMockData";
+// import { patients } from "./utils/patientsMockData";
+import { Patient } from "@/lib/services/PatientsTypes";
 
-const PatientList = () => {
-    return (
+const PatientList = ({ patients }: { patients: Patient[] }) => {
+  return (
     <ul role="list" className="rounded-3xl bg-white divide-y divide-gray-100">
-        <li className="flex justify-between items-center gap-x-6 p-5">
-            <h2 className="text-2xl font-medium" >Patients</h2>
-            <SearchIcon />
-        </li>
-        { patients.map( (patient, index) => {
-            return (
-                <li 
-                key={index}
-                className={ patient.isActive ? 
-                    "flex justify-between gap-x-6 p-5 items-center bg-gray-200" :
-                    "flex justify-between gap-x-6 p-5 items-center"
-                }>
-                    <div className="flex min-w-0 gap-x-4">
-                    <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={patient.image} alt="" />
-                    <div className="min-w-0 flex-auto">
-                        <p className="text-sm font-semibold leading-6 text-gray-900">{patient.name}</p>
-                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">{patient.gender}, {patient.age}</p>
-                    </div>
-                    </div>
-                    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-center">
-                        <MenuIcon/>
-                    </div>
-                </li>
-            )
-        })}
+      <li className="flex justify-between items-center gap-x-6 p-5">
+        <h2 className="text-2xl font-medium">Patients</h2>
+        <SearchIcon />
+      </li>
+      {patients?.map((patient, index) => {
+        return (
+          <li
+            key={index}
+            // className={
+            //   patient.isActive
+            //     ? "flex justify-between gap-x-6 p-5 items-center bg-gray-200"
+            //     : "flex justify-between gap-x-6 p-5 items-center"
+            // }
+            className="flex justify-between gap-x-6 p-5 items-center"
+          >
+            <div className="flex min-w-0 gap-x-4">
+              <img
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                src={patient.profile_picture}
+                alt={patient.name}
+              />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {patient.name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {patient.gender}, {patient.age}
+                </p>
+              </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-center">
+              <MenuIcon />
+            </div>
+          </li>
+        );
+      })}
     </ul>
-    )
-}
+  );
+};
 
-export default PatientList
+export default PatientList;
--- lib/services/Patients.ts
+++ lib/services/Patients.ts
@@ -1,24 +1,31 @@
-import { API_URL, API_AUTHORIZATION_USERNAME, API_AUTHORIZATION_PASSWORD } from "./const";
-import { Patient } from './PatientsTypes';
+import {
+  API_URL,
+  API_AUTHORIZATION_USERNAME,
+  API_AUTHORIZATION_PASSWORD,
+} from "./const";
+import { Patient } from "./PatientsTypes";
 
-const auth = btoa(`${API_AUTHORIZATION_USERNAME}:${API_AUTHORIZATION_PASSWORD}`)
+const auth = btoa(
+  `${API_AUTHORIZATION_USERNAME}:${API_AUTHORIZATION_PASSWORD}`
+);
 
-const getAllPatients : () => Promise<Patient[]> = async function () {
+const getAllPatients: () => Promise<Patient[]> = async function () {
+  const res = await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${auth}`,
+    },
+  });
 
-    const res = await fetch(API_URL, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Basic ${auth}`
-        }
-    })
+  console.log("Authorization", `Basic ${auth}`);
 
-    console.log("Authorization", `Basic ${auth}`)
+  if (!res.ok)
+    throw new Error(
+      `Error fetching Patient API Data ${(res.status, res.statusText)}`
+    );
 
-    if (!res.ok)
-        throw new Error(`Error fetching Patient API Data ${res.status, res.statusText}`, );
+  return res.json();
+};
 
-    return res.json()
-} 
-
-export default getAllPatients
+export default getAllPatients;
